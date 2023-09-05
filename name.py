bb9f1eb8a3e318b83616b0398854a329543fdc9f
Диана Петрова <petrovadi123@gmail.com>
2023-09-05 21:47:27 +0300

    Условие

--- name.py
+++ name.py
@@ -3,3 +3,12 @@ import random
 user=int(input("Введите двухзначное целое число: "))
 comp=random.randint(10,100)
 
+while 10<=user<100:
+    if user>comp:
+        print("Вы -",user,"Комп -",comp,"ИТОГ:Победа")
+    else:
+        print("Вы -",user,"Комп -",comp,"ИТОГ:Проигрыш")
+    break
+else:
+    print("Введите целое число!")
+    user = int(input("Введите двухзначное целое число: "))
